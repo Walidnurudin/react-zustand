40ba5ee17277e2b2791a001345ca6eb9f2ad9f29
walidnurudin <walidnurudin47@gmail.com>
2022-07-13 14:17:38 +0700

    persist & devtools

--- src/store/useUser.js
+++ src/store/useUser.js
@@ -1,12 +1,23 @@
 import create from 'zustand';
+import { persist, devtools } from 'zustand/middleware';
 
-const useUser = create((set) => ({
-  user: {},
-  isLogin: false,
+const useUser = create(
+  devtools(
+    persist(
+      (set) => ({
+        user: {},
+        isLogin: false,
 
-  setUser: (data) => set(() => ({ user: data })),
-  setIsLogin: set(() => ({ isLogin: true })),
-  removeUser: set(() => ({ user: {}, isLogin: false }), true),
-}));
+        setUser: (data) => set(() => ({ user: data })),
+        setIsLogin: set(() => ({ isLogin: true })),
+        removeUser: set(() => ({ user: {}, isLogin: false }), true),
+      }),
+      {
+        name: 'user',
+        getStorage: () => localStorage,
+      }
+    )
+  )
+);
 
 export default useUser;
