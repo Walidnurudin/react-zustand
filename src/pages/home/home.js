1e2fec19fd6bebfc7b9cc9a1b0970d829f57f11c
walidnurudin <walidnurudin47@gmail.com>
2022-07-16 12:04:37 +0700

    lottie

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Lottie from 'react-lottie-player';
+
+import lottieJson from '../../components/lottie/lottie.json';
+import lottieRamdhan from '../../components/lottie/ramadhan.json';
 
 function home() {
-  return <div>home</div>;
+  return (
+    <div>
+      home
+      <Lottie
+        loop
+        animationData={lottieJson}
+        play
+        style={{ width: 150, height: 150 }}
+      />
+      <Lottie
+        loop
+        animationData={lottieRamdhan}
+        play
+        // style={{ width: 200, height: 300 }}
+      />
+    </div>
+  );
 }
 
 export default home;
